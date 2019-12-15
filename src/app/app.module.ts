32b926bedcc17a16b3050d0ccf4bd45fca081849
Julio A. Mistral <julio.mistral@gmail.com>
2019-12-14 22:54:04 -0800

    1) Added Bootstrap JS to project 2) Added Angular Material to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
 import { HomeComponent } from '@src/app/home/home.component';
+import { TaskListComponent } from '@src/app/home/task-list/task-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    TaskListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
