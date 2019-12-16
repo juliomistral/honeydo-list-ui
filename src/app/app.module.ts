198e3bbf67a3b96cb52e53ca41aa2fc33342ae49
Julio Mistral <jmistral@jmistral-macbookpro.roam.corp.google.com>
2019-12-16 15:36:30 -0800

    Moved task list component out of home component directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
 import { HomeComponent } from '@src/app/home/home.component';
-import { TaskListComponent } from '@src/app/home/task-list/task-list.component';
+import { TaskListComponent } from '@src/app/task-list/task-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTreeModule } from '@angular/material';
 import { ReactiveFormsModule } from '@angular/forms';
